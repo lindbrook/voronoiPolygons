21e8d29c9771e2cb0d211a9fc894cf68b8bcfb44
lindbrook <lindbrook@gmail.com>
2018-12-12 05:04:54 -0800

    use rw.data as argument

--- R/deldirPolygons.R
+++ R/deldirPolygons.R
@@ -2,7 +2,7 @@
 #'
 #' For use with polygon functions.
 #' @param sites Object. Data frame of sites to construct Voronoi diagram with variables "x" and "y".
-#' @param observed.data Object. Data frame of secondary source of data: observations, cases, addresses, etc. with variables "x" and "y".
+#' @param rw.data Object. Data frame of secondary source of data for rectangular window or bounding box: observations, cases, addresses, etc. with variables "x" and "y".
 #' @param rw Numeric. Vector of corners of the rectangular window or bounding box: xmin, xmax, ymin, ymax. For deldir::deldir().
 #' @export
 #' @examples
@@ -14,17 +14,17 @@
 #' cholera::snowMap()
 #' invisible(lapply(polygon.vertices, polygon))
 
-deldirPolygons <- function(sites, observed.data = NULL, rw = NULL) {
-  if (is.null(observed.data) & is.null(rw)) {
+deldirPolygons <- function(sites, rw.data = NULL, rw = NULL) {
+  if (is.null(rw.data) & is.null(rw)) {
     x.rng <- range(sites$x)
     y.rng <- range(sites$y)
-  } else if (is.null(observed.data) == FALSE & is.null(rw)) {
-    x.rng <- range(observed.data$x)
-    y.rng <- range(observed.data$y)
-  } else if (is.null(observed.data) & is.null(rw) == FALSE) {
+  } else if (is.null(rw.data) == FALSE & is.null(rw)) {
+    x.rng <- range(rw.data$x)
+    y.rng <- range(rw.data$y)
+  } else if (is.null(rw.data) & is.null(rw) == FALSE) {
     x.rng <- rw[1:2]
     y.rng <- rw[3:4]
-  } else stop("Use either 'observed.data' or 'rw'; not both.")
+  } else stop("Use either 'rw.data' or 'rw'; not both.")
 
   voronoi <- deldir::deldir(sites[, c("x", "y")], rw = c(x.rng, y.rng),
     suppressMsge = TRUE)
