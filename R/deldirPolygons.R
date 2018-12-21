903869bf8213b46fe5dc3103b1df5403f87e9b55
lindbrook <lindbrook@gmail.com>
2018-12-21 09:08:59 -0800

    add Delauny triangulation

--- R/deldirPolygons.R
+++ R/deldirPolygons.R
@@ -1,9 +1,10 @@
-#' Compute vertices of Voronoi diagram tiles.
+#' Extract vertices of Delauny triangles and Dirichelet (Voronoi) tiles.
 #'
-#' For use with polygon functions.
-#' @param sites Object. Data frame of sites to construct Voronoi diagram with variables "x" and "y".
-#' @param rw.data Object. Data frame of secondary source of data for rectangular window or bounding box: observations, cases, addresses, etc. with variables "x" and "y".
-#' @param rw Numeric. Vector of corners of the rectangular window or bounding box: xmin, xmax, ymin, ymax. For deldir::deldir().
+#' For use with functions that use polygons.
+#' @param sites Object. Data frame of sites to compute Delauny triangulation and Dirichelet (Voronoi) tessellation with variables "x" and "y".
+#' @param rw.data Object. Data frame of secondary source of data to set the rectangular window or bounding box: observations, cases, etc. with variables "x" and "y".
+#' @param rw Numeric. Vector of corners to define the rectangular window or bounding box: xmin, xmax, ymin, ymax. For deldir::deldir().
+#' @param type Character. Soure of polygons: "tiles" or "triangles".
 #' @export
 #' @examples
 #' polygon.vertices <- deldirPolygons(cholera::pumps)
@@ -13,8 +14,17 @@
 #' polygon.vertices <- deldirPolygons(cholera::pumps, cholera::roads)
 #' cholera::snowMap()
 #' invisible(lapply(polygon.vertices, polygon))
+#'
+#' polygon.vertices <- deldirPolygons(cholera::pumps, cholera::roads,
+#'  type = "triangles")
+#' cholera::snowMap()
+#' invisible(lapply(polygon.vertices, polygon))
 
-deldirPolygons <- function(sites, rw.data = NULL, rw = NULL) {
+deldirPolygons <- function(sites, rw.data = NULL, rw = NULL, type = "tiles") {
+  if (type %in% c("tiles", "triangles") == FALSE) {
+    stop('type must be "tiles" or "triangles".')
+  }
+
   if (is.null(rw.data) & is.null(rw)) {
     x.rng <- range(sites$x)
     y.rng <- range(sites$y)
@@ -24,10 +34,16 @@ deldirPolygons <- function(sites, rw.data = NULL, rw = NULL) {
   } else if (is.null(rw.data) & is.null(rw) == FALSE) {
     x.rng <- rw[1:2]
     y.rng <- rw[3:4]
-  } else stop("Use either 'rw.data' or 'rw'; not both.")
+  } else stop("Use either 'rw.data' or 'rw', not both.")
 
-  voronoi <- deldir::deldir(sites[, c("x", "y")], rw = c(x.rng, y.rng),
+  tile.triangle <- deldir::deldir(sites[, c("x", "y")], rw = c(x.rng, y.rng),
     suppressMsge = TRUE)
-  cell.data <- deldir::tile.list(voronoi)
-  lapply(cell.data, function(dat) data.frame(x = dat$x, y = dat$y))
+
+  if (type == "tiles") {
+    polygon.data <- deldir::tile.list(tile.triangle)
+  } else if (type == "triangles") {
+    polygon.data <- deldir::triang.list(tile.triangle)
+  }
+
+  lapply(polygon.data, function(dat) data.frame(x = dat$x, y = dat$y))
 }
